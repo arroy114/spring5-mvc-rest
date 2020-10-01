f587ebc7bbf0c837fc9c03a3f031330e0193a747
Roy Hung <43883872+arroy114@users.noreply.github.com>
2020-10-01 13:02:31 +0200

    Added Mapstruct

--- src/main/java/guru/springfamework/repositories/CategoryRepository.java
+++ src/main/java/guru/springfamework/repositories/CategoryRepository.java
@@ -6,5 +6,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 /**
  * Created by jt on 9/24/17.
  */
+//JpaRepository extends PagingAndSortingRepository which extends CrudRepository
+    //=> give you some extra functions
 public interface CategoryRepository extends JpaRepository<Category, Long> {
 }
